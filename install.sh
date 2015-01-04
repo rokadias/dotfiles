ffa72f141e93108e1d6983286973bc8e88813a0f
Saqib Rokadia <saqib@saqibr.com>
2015-01-04 14:56:43 -0800

    Initial Creation and Addition of existing dotfiles.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir ~/.xmonad/
+ln -s $PWD/xmonad/xmonad.hs ~/.xmonad/
+
+ln -s $PWD/emacs/emacs ~/.emacs
+mkdir ~/.emacs.d
+
+ln -s $PWD/home/bashrc ~/.bashrc
+ln -s $PWD/home/bash_profile ~/.bash_profile
+ln -s $PWD/home/conkyrc ~/.conkyrc
+ln -s $PWD/home/xinitrc ~/.xinitrc
+ln -s $PWD/home/xmodmap ~/.xmodmap
+ln -s $PWD/home/xprofile ~/.xprofile
+ln -s $PWD/home/zprofile ~/.zprofile
+ln -s $PWD/home/zshenv ~/.zshenv
+ln -s $PWD/home/zshrc ~/.zshrc
+
+sudo ln -s $PWD/etc/pacman.conf /etc/
+sudo ln -s $PWD/etc/pacman.d/mirrorlist /etc/pacman.d/
+
+sudo cp $PWD/etc/systemd/system/docker.service /etc/systemd/system/
+sudo cp $PWD/etc/systemd/user/evincer.service /etc/systemd/user/
