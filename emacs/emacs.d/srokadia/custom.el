9d6c4237c30c2cbc113f54fd6bb930d762d5ebda
Saqib Rokadia <srokadia@SaqibLaptop.localdomain>
2015-01-08 20:11:06 -0800

    Chanes from the laptop.

--- emacs/emacs.d/srokadia/custom.el
+++ emacs/emacs.d/srokadia/custom.el
@@ -10,7 +10,7 @@
  '(case-replace nil)
  '(custom-safe-themes
    (quote
-    ("b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "a233249cc6f90098e13e555f5f5bf6f8461563a8043c7502fb0474be02affeea" "86f4407f65d848ccdbbbf7384de75ba320d26ccecd719d50239f2c36bec18628" "16248150e4336572ff4aa21321015d37c3744a9eb243fbd1e934b594ff9cf394" "4217c670c803e8a831797ccf51c7e6f3a9e102cb9345e3662cc449f4c194ed7d" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "6e05b0a83b83b5efd63c74698e1ad6feaddf69a50b15a8b4a83b157aac45127c" default)))
+    ("a233249cc6f90098e13e555f5f5bf6f8461563a8043c7502fb0474be02affeea" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "6e05b0a83b83b5efd63c74698e1ad6feaddf69a50b15a8b4a83b157aac45127c" default)))
  '(erc-irccontrols-mode t)
  '(fci-rule-color "#202325")
  '(ido-vertical-mode t)
@@ -42,4 +42,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
