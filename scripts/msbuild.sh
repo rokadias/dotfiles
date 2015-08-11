4d2ffe521199a06ac31353b8889f27c8e19bcddf
Saqib Rokadia <srokadia@SaqibLaptop.localdomain>
2015-08-10 18:35:24 -0700

    Updating msbuild script to use rsync to build code on remote machine for msbuild. So that I can run emacs on linux while compiling on windows.

--- scripts/msbuild.sh
+++ scripts/msbuild.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 filepath=$1
+localdirpath=${filepath%/*}
 filepath=$(echo $filepath | sed -e 's/\/home\/srokadia/~/g')
 dirpath=${filepath%/*}
+# dirpath=$(dirname $dirpath)
 
 echo "Starting ssh win dospath."
-dosdirpath=$(ssh win "cygpath -w $dirpath")
+dosdirpath=$(ssh dev.win "cygpath -w $dirpath")
 echo "Finished ssh win dospath."
+
+rsyncpath=$(echo $dirpath | sed -e 's/~\///g')
+echo "Starting rsync win."
+rsync -a $localdirpath/* dev.win:$rsyncpath
+echo "Finished rsync win."
 
 regexdosdir=$(echo -E "$dosdirpath\\" | sed -e 's/\\/\\\\/g')
 echo "Starting MSBuild."
-/bin/time ssh win "MSBuild.exe /m /nodereuse:false /v:q /p:GenerateFullPaths=true \$(cygpath -w $filepath) /p:StyleCop=false /p:BuildProjectReferences=true" | sed -e "s/$regexdosdir//Ig" -e 's/\\/\//g'
+/bin/time ssh dev.win "TEMP=/tmp MSBuild.exe /m /nodereuse:false /v:q /p:GenerateFullPaths=true \$(cygpath -w $filepath) /p:StyleCop=false /p:BuildProjectReferences=true" | sed -e "s/$regexdosdir//Ig" -e 's/\\/\//g'
 echo "Finished MSBuild."
 cd $(dirname $1)
-
