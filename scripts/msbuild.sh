1b781171b5e3690f40c6341812e8af52ee02ed32
Saqib Rokadia <saqib@saqibr.com>
2015-09-25 10:36:00 -0700

    *** empty log message ***

--- scripts/msbuild.sh
+++ scripts/msbuild.sh
@@ -11,6 +11,12 @@ echo "Starting ssh win dospath."
 dosdirpath=$(ssh dev.win "cygpath -w ~")
 echo "Finished ssh win dospath."
 
+echo $filepath | grep 'test.csproj$'
+if [ $? -eq 0 ] && [ -d "$localdirpath/test/" ]
+then
+    rm -rf $localdirpath/test/bin/
+fi
+
 rsyncpath=$(echo $dirpath | sed -e 's/~\///g')
 echo "Starting rsync win."
 rsync -az --filter="merge /home/srokadia/scripts/rsyncignore" --delete $localdirpath/* dev.win:$rsyncpath
@@ -20,4 +26,13 @@ regexdosdir=$(echo -E "$dosdirpath\\" | sed -e 's/\\/\\\\/g')
 echo "Starting MSBuild."
 /bin/time ssh dev.win "TEMP=/tmp MSBuild.exe /m /nodereuse:false /v:q /p:GenerateFullPaths=true \$(cygpath -w $filepath) /p:StyleCop=false /p:BuildProjectReferences=true" | sed -e "s/$regexdosdir/~\//Ig" -e 's/\\/\//g'
 echo "Finished MSBuild."
+
+echo $filepath | grep 'test.csproj$'
+if [ $? -eq 0 ] && [ -d "$localdirpath/test/" ]
+then
+    echo "Starting rsync test folder."
+    mkdir $localdirpath/test/bin/
+    rsync -az --filter="merge /home/srokadia/scripts/rsyncignore" --delete dev.win:$rsyncpath/test/bin/* $localdirpath/test/bin/
+fi  
+
 cd $(dirname $1)
