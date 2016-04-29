f1e3224891fd9a2991e2c5f371ec49b84bae6409
Saqib Rokadia <saqib@saqibr.com>
2016-04-29 10:39:41 -0700

    Adding indentation rules for web-mode.

--- emacs/emacs.d/srokadia/html.el
+++ emacs/emacs.d/srokadia/html.el
@@ -7,3 +7,11 @@
 
 (add-hook 'nxml-mode-hook 'on-nxml-loaded)
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
