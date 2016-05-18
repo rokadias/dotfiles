657ffea5d9fcf4dffc7dd4b611d793eadc53347a
Saqib Rokadia <saqib@saqibr.com>
2016-05-18 14:02:18 -0700

    Removing packages that I don't think I need any more.

--- emacs/emacs.d/elpa/ercn-20150523.803/ercn-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ercn" "20150523.803" "Flexible ERC notifications" 'nil :url "http://www.github.com/leathekd/ercn")
--- emacs/emacs.d/elpa/jabber-20160124.552/jabber-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "jabber" "20160124.552" "A Jabber client for Emacs."
-  '((fsm "0.2")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
