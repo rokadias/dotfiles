afab434194d728a5651f4c0f80505c6834e73007
Saqib Rokadia <saqib@invioinc.com>
2018-04-16 11:09:50 -0700

    Adding additional packages.

---
+++ emacs/emacs.d/elpa/yaxception-20150105.652/yaxception-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yaxception" "20150105.652" "Provide framework about exception like Java for Elisp" 'nil :commit "4e94cf3e0b9b5631b0e90eb4b7de597ee7185875" :url "https://github.com/aki2o/yaxception" :keywords '("exception" "error" "signal"))
