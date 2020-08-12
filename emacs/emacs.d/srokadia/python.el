559604150ce9586318292b1408789a13365596ef
Saqib Rokadia <saqib@saqibr.com>
2020-08-12 08:43:49 -0700

    Adding support for lsp python.

--- emacs/emacs.d/srokadia/python.el
+++ emacs/emacs.d/srokadia/python.el
@@ -1,2 +1,5 @@
-(require 'lsp-mode)
+(require 'lsp-python-ms)
+(setq lsp-python-ms-executable "~/OpenSource/python-language-server/output/bin/Release/linux-x64/publish/Microsoft.Python.LanguageServer")
+
 (add-hook 'python-mode-hook #'lsp)
+(add-hook 'rust-mode-hook #'company-mode)
