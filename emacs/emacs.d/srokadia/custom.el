ab955243f96b18b62fb2d4ceaeeafd0c32329554
Saqib Rokadia <saqib@saqibr.com>
2021-03-31 13:32:40 -0700

    Allowing downcasing a region.

--- emacs/emacs.d/srokadia/custom.el
+++ emacs/emacs.d/srokadia/custom.el
@@ -56,3 +56,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:background nil)))))
+
+(put 'downcase-region 'disabled nil)
