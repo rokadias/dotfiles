45f4d10c3482cdac95e75ae57e3b2ff242a7c8c6
Saqib Rokadia <saqib@saqibr.com>
2021-04-04 09:02:45 -0700

    Adding a couple of new packages

---
+++ emacs/emacs.d/elpa/polymode-20200606.1106/polymode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "polymode" "20200606.1106" "Extensible framework for multiple major modes"
+  '((emacs "25"))
+  :commit "b3871e946d278a256d95a3bd4615b1df234ad75a" :authors
+  '(("Vitalie Spinu"))
+  :maintainer
+  '("Vitalie Spinu")
+  :keywords
+  '("languages" "multi-modes" "processes")
+  :url "https://github.com/polymode/polymode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
