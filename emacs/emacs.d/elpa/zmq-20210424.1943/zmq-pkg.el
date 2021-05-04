af9472f146e79bcccf5b70ec534bcf9f462037bd
Saqib Rokadia <saqib@saqibr.com>
2021-05-03 21:44:20 -0700

    Adding jupyter to pakcage list.

---
+++ emacs/emacs.d/elpa/zmq-20210424.1943/zmq-pkg.el
@@ -0,0 +1,13 @@
+(define-package "zmq" "20210424.1943" "ZMQ bindings in elisp"
+  '((cl-lib "0.5")
+    (emacs "26"))
+  :commit "790033363cf0e78c616cfe117a2f681381e96f29" :authors
+  '(("Nathaniel Nicandro" . "nathanielnicandro@gmail.com"))
+  :maintainer
+  '("Nathaniel Nicandro" . "nathanielnicandro@gmail.com")
+  :keywords
+  '("comm")
+  :url "https://github.com/nnicandro/emacs-zmq")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
