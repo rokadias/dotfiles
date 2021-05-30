95533cbe8953f2b15b9707256a762233c3886c4f
Saqib Rokadia <saqib@saqibr.com>
2021-05-30 12:59:07 -0700

    Changing font to be Fira code with ligatures.

--- emacs/emacs.d/srokadia/w32.el
+++ emacs/emacs.d/srokadia/w32.el
@@ -13,4 +13,4 @@
   (setq w32-get-true-file-attributes nil))
 
 (unless (or is-w32 is-cygwin)
-  (modify-all-frames-parameters (list (cons 'font "DejaVu Sans Mono-9"))))
+  (modify-all-frames-parameters (list (cons 'font "Fira Code 12"))))
