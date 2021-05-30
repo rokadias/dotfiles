ef7ac9293c8f9d37d69fb52c29621ff48c77052d
Saqib Rokadia <saqib@saqibr.com>
2021-05-30 11:52:32 -0700

    Making the install process idempotent.

--- install.sh
+++ install.sh
@@ -1,82 +1,90 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir ~/scripts
-ln -s $PWD/scripts/dbuild.sh ~/scripts/dbuild.sh
-ln -s $PWD/scripts/dtest.sh ~/scripts/dtest.sh
-ln -s $PWD/scripts/dweb.sh ~/scripts/dweb.sh
-ln -s $PWD/scripts/msbuild.sh ~/scripts/msbuild.sh
-ln -s $PWD/scripts/fullmsbuild.sh ~/scripts/fullmsbuild.sh
-ln -s $PWD/scripts/rsyncignore ~/scripts/rsyncignore
-ln -s $PWD/scripts/nunit.sh ~/scripts/nunit.sh
-ln -s $PWD/scripts/xunit.sh ~/scripts/xunit.sh
+set -e
 
-mkdir ~/.xmonad/
-ln -s $PWD/home/xmonad/xmonad.hs ~/.xmonad/
+DOTFILES_DIR=$(realpath $(dirname $BASH_SOURCE[0]))
+source $DOTFILES_DIR/exesudo.sh
 
-ln -s $PWD/emacs/emacs ~/.emacs
-mkdir -p ~/.emacs.d/srokadia
-ln -s $PWD/emacs/emacs.d/srokadia/autocomplete.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/bindings.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/compile.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/csharp.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/css.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/custom.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/erc.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/find-file-in-repository.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/flycheck.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/generalfunctions.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/git.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/global.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/handlebars.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/haskell.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/hg.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/html.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/javascript.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/jsx.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/key-chord.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/notify.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/org.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/package.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/powershell.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/progmodefunctions.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/python.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/ra-emacs-lsp.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/ra-lsp.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/restclient.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/rust.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/session.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/shell.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/theme.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/tramp.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/yaml.el ~/.emacs.d/srokadia/
-ln -s $PWD/emacs/emacs.d/srokadia/yasnippet.el ~/.emacs.d/srokadia/
+linkIfNotExists() {
+    local SOURCE=$1
+    local DEST_PATH=$2
+    local PREFIX=$3
+    local BASENAME=$(basename $SOURCE)
+    local DESTINATION=$DEST_PATH/$PREFIX$BASENAME
 
-ln -s $PWD/emacs/emacs.d/elpa ~/.emacs.d/
-ln -s $PWD/emacs/emacs.d/lisp ~/.emacs.d/
-ln -s $PWD/emacs/emacs.d/snippets ~/.emacs.d/
+    if [[ ( -f "$SOURCE" ) && ( ! -f "$DESTINATION" ) ]] || [[ ( -d "$SOURCE" ) && ( ! -d "$DESTINATION" ) ]]; then
+        echo "Linking: $SOURCE to $DESTINATION"
+        ln -s $SOURCE $DESTINATION
+    fi
+}
+export linkIfNotExists
 
-ln -s $PWD/home/bashrc ~/.bashrc
-ln -s $PWD/home/bash_profile ~/.bash_profile
-ln -s $PWD/home/conkyrc ~/.conkyrc
-ln -s $PWD/home/eslintrc ~/.eslintrc
-ln -s $PWD/home/xinitrc ~/.xinitrc
-ln -s $PWD/home/xmobarrc ~/.xmobarrc
-ln -s $PWD/home/xmodmap ~/.xmodmap
-ln -s $PWD/home/xprofile ~/.xprofile
-ln -s $PWD/home/zprofile ~/.zprofile
-ln -s $PWD/home/zshenv ~/.zshenv
-ln -s $PWD/home/zshrc ~/.zshrc
 
-sudo ln -s $PWD/etc/pacman.conf /etc/
-sudo ln -s $PWD/etc/pacman.d/mirrorlist /etc/pacman.d/
+linkFilesInDotfilesToPath() {
+    local SOURCE_PATH=$DOTFILES_DIR/$1
+    local DEST_PATH=$2
+    local PREFIX=$3
 
-sudo cp $PWD/etc/systemd/system/clean_tmp.service /etc/systemd/system/
-sudo cp $PWD/etc/systemd/system/clean_tmp.timer /etc/systemd/system/
-sudo cp $PWD/etc/systemd/user/skydns.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/skydock.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/evincer.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/nexxusblog.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/trunk-mysql.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/redis-container.service /etc/systemd/user/
-sudo cp $PWD/etc/systemd/user/rewards-max.service /etc/systemd/system/
-sudo cp $PWD/etc/systemd/user/rewards-max.timer /etc/systemd/system/
+    FILES=$(find $SOURCE_PATH -maxdepth 1 -type f )
+
+    for f in $FILES
+    do
+        linkIfNotExists $f $DEST_PATH $PREFIX
+    done
+}
+export linkFilesInDotfilesToPath
+
+linkDirectoriesInDotfilesToPath() {
+    local SOURCE_PATH=$DOTFILES_DIR/$1
+    local DEST_PATH=$2
+    local PREFIX=$3
+
+    DIRECTORIES=$(find $SOURCE_PATH -mindepth 1 -maxdepth 1 -type d)
+
+    for d in $DIRECTORIES
+    do
+        linkIfNotExists $d $DEST_PATH $PREFIX
+    done
+}
+export linkDirectoriesInDotfilesToPath
+
+echo $'\nLooking through home/xmonad/'
+mkdir -p $HOME/scripts
+linkFilesInDotfilesToPath scripts $HOME/scripts/
+
+# Xmonad
+echo $'\nLooking through home/xmonad/'
+mkdir -p $HOME/.xmonad
+linkFilesInDotfilesToPath home/xmonad/ $HOME/.xmonad
+
+# emacs
+echo $'\nLooking through emacs/'
+mkdir -p $HOME/.emacs.d
+linkIfNotExists $DOTFILES_DIR/emacs/emacs $HOME "."
+linkDirectoriesInDotfilesToPath emacs/emacs.d $HOME/.emacs.d
+
+# Home dotfiles
+echo $'\nLooking through home/'
+linkFilesInDotfilesToPath home/ $HOME "."
+
+echo $'\nLooking through etc/pacman and etc/systemd/system'
+if [[ $(whoami) == "root" ]]; then
+    mkdir -p /etc/pacman.d
+    linkIfNotExists $DOTFILES_DIR/etc/pacman.conf /etc
+    linkIfNotExists $DOTFILES_DIR/etc/pacman.d/mirrorlist /etc/pacman.d
+
+    cp $DOTFILES_DIR/etc/systemd/system/tmpfiles-cleanup.service /etc/systemd/system
+    cp $DOTFILES_DIR/etc/systemd/system/tmpfiles-cleanup.timer /etc/systemd/system
+else
+    sudo mkdir -p /etc/pacman.d
+    exesudo linkIfNotExists $DOTFILES_DIR/etc/pacman.conf /etc
+    exesudo linkIfNotExists $DOTFILES_DIR/etc/pacman.d/mirrorlist /etc/pacman.d
+
+    sudo cp $DOTFILES_DIR/etc/systemd/system/tmpfiles-cleanup.service /etc/systemd/system
+    sudo cp $DOTFILES_DIR/etc/systemd/system/tmpfiles-cleanup.timer /etc/systemd/system
+fi
+
+echo $'\nLooking through etc/systemd/user'
+if [[ -d $HOME/.local/share/systemd/user ]]; then
+    find etc/systemd/user/ -maxdepth 1 -type p -exec cp {} $HOME/.local/share/systemd/user \;
+fi
