b11597292958f5458d8a6f0c2a5c5e74a484ac91
Saqib Rokadia <saqib@saqibr.com>
2021-06-06 19:35:05 -0700

    Using lsp-pyright instead of python-language-server.

--- emacs/emacs.d/srokadia/python.el
+++ emacs/emacs.d/srokadia/python.el
@@ -1,7 +1,11 @@
 ;; (require 'lsp-python-ms)
 ;; (setq lsp-python-ms-executable "~/OpenSource/python-language-server/output/bin/Release/linux-x64/publish/Microsoft.Python.LanguageServer")
 
-(add-hook 'python-mode-hook #'lsp)
-(add-hook 'rust-mode-hook #'company-mode)
+(defun on-python-load ()
+  (require 'lsp-pyright)
+  (lsp))
+(add-hook 'python-mode-hook #'on-python-load)
+
+
 (with-eval-after-load "semantic/idle"
   (advice-add 'semantic-idle-scheduler-function :around #'ignore))
