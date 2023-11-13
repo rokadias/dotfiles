c8b77e1e0549c379a00d638260aa22256846772c
Saqib Rokadia <saqib@falkon.ai>
2023-11-12 22:20:30 -0800

    Updating packages and including flymake-ruff.

---
+++ emacs/emacs.d/elpa/flymake-ruff-20230620.115/flymake-ruff-autoloads.el
@@ -0,0 +1,29 @@
+;;; flymake-ruff-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-ruff" "flymake-ruff.el" (0 0 0 0))
+;;; Generated autoloads from flymake-ruff.el
+
+(autoload 'flymake-ruff-load "flymake-ruff" "\
+Load hook for the current buffer to tell flymake to run checker." t nil)
+
+(register-definition-prefixes "flymake-ruff" '("flymake-ruff-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("flymake-ruff-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-ruff-autoloads.el ends here
---
+++ emacs/emacs.d/elpa/flymake-ruff-20230620.115/flymake-ruff.el
@@ -0,0 +1,87 @@
+;;; flymake-ruff.el --- A flymake plugin for python files using ruff
+
+;; Copyright © 2023 Erick Navarro
+;; Author: Erick Navarro <erick@navarro.io>
+;; URL: https://github.com/erickgnavar/flymake-ruff
+;; Version: 0.1.0
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Package-Requires: ((emacs "26.1") (project "0.3.0"))
+
+;;; Commentary:
+
+;; Usage:
+;;   (require 'flymake-ruff)
+;;   (add-hook 'python-mode-hook #'flymake-ruff-load)
+;;
+;; Or, with use-package:
+;;   (use-package flymake-ruff
+;;     :ensure t
+;;     :hook (python-mode . flymake-ruff-load))
+
+;;; Code:
+
+(require 'project)
+
+(defcustom flymake-ruff-program "ruff"
+  "Path to program ruff."
+  :group 'flymake-ruff
+  :type 'string)
+
+(defcustom flymake-ruff-program-args
+  '("--format" "text" "--exit-zero" "--quiet" "-")
+  "Flags to be given to \"ruff\"."
+  :group 'flymake-ruff
+  :type '(list string))
+
+(defvar flymake-ruff--output-regex "\\(.*\\):\\([0-9]+\\):\\([0-9]+\\): \\([A-Z0-9]+\\) \\(.*\\)")
+
+(defconst flymake-ruff--default-configs
+  '("pyproject.toml" "ruff.toml" ".ruff.toml")
+  "Default configuration files supported by Ruff.")
+
+(defun flymake-ruff--check-buffer ()
+  "Generate a list of diagnostics for the current buffer."
+  (let ((code-buffer (current-buffer))
+        (code-content (buffer-substring-no-properties (point-min) (point-max)))
+        (dxs '()))
+    (with-temp-buffer
+      (insert code-content)
+      (let* ((config (seq-find
+                      #'file-readable-p
+                      (mapcar
+                       (lambda (f)
+                         (expand-file-name f (project-root (project-current))))
+                       flymake-ruff--default-configs)))
+             (args (if config
+                       (append `("--config" ,config)
+                               flymake-ruff-program-args)
+                     flymake-ruff-program-args)))
+        ;; call-process-region will run the program and replace current buffer
+        ;; with its stdout, that's why we need to run it in a temporary buffer
+        (apply #'call-process-region (point-min) (point-max) flymake-ruff-program t t nil args))
+      (goto-char (point-min))
+      (while (search-forward-regexp flymake-ruff--output-regex (point-max) t)
+        (when (match-string 2)
+          (let* ((line (string-to-number (match-string 2)))
+                 (col (string-to-number (match-string 3)))
+                 (code (match-string 4))
+                 (msg (match-string 5))
+                 (description (format "Ruff: %s %s" code msg))
+                 (region (flymake-diag-region code-buffer line col))
+                 (dx (flymake-make-diagnostic code-buffer (car region) (cdr region)
+                                              :error description)))
+            (add-to-list 'dxs dx)))))
+    dxs))
+
+;;;###autoload
+(defun flymake-ruff-load ()
+  "Load hook for the current buffer to tell flymake to run checker."
+  (interactive)
+  (add-hook 'flymake-diagnostic-functions #'flymake-ruff--run-checker nil t))
+
+(defun flymake-ruff--run-checker (report-fn &rest _args)
+  "Run checker using REPORT-FN."
+  (funcall report-fn (flymake-ruff--check-buffer)))
+
+(provide 'flymake-ruff)
+;;; flymake-ruff.el ends here
