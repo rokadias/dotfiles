f6841b69c7046b25484981767b879dc4506e6953
Saqib Rokadia <saqib@saqibr.com>
2024-05-22 12:31:37 -0700

    Including elpa packages updates.

--- emacs/emacs.d/elpa/helm-ls-git-20230621.1538/helm-ls-git-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "helm-ls-git" "20230621.1538" "list git files."
-  '((helm "1.7.8"))
-  :commit "78418a73ca7e796bc95da47a0f3d6bfa0af382b5")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- emacs/emacs.d/elpa/lv-20200507.1518/lv.el
+++ emacs/emacs.d/elpa/lv-20200507.1518/lv.el
@@ -1,6 +1,4 @@
 ;;; lv.el --- Other echo area
-;; Package-Version: 20200507.1518
-;; Package-Commit: 2d553787aca1aceb3e6927e426200e9bb9f056f1
 
 ;; Copyright (C) 2015  Free Software Foundation, Inc.
 
