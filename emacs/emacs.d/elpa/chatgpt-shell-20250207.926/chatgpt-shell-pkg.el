701fe28fea9c311ccb2516118d7cc3a2f30eca94
Saqib Rokadia <saqib@saqibr.com>
2025-05-23 20:59:52 -0700

    Adding chatgpt shell.

---
+++ emacs/emacs.d/elpa/chatgpt-shell-20250207.926/chatgpt-shell-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "chatgpt-shell" "20250207.926"
+  "A multi-LLM shell (ChatGPT, Claude, Gemini, Kagi, Ollama, Perplexity) + editing integrations."
+  '((emacs       "28.1")
+    (shell-maker "0.76.2"))
+  :url "https://github.com/xenodium/chatgpt-shell"
+  :commit "be81dd78fd1a6e7f0e50a0e7d67c0542d47783a8"
+  :revdesc "be81dd78fd1a")
---
+++ emacs/emacs.d/elpa/shell-maker-20241226.2102/shell-maker-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "shell-maker" "20241226.2102"
+  "Interaction mode for making comint shells."
+  '((emacs "27.1"))
+  :url "https://github.com/xenodium/shell-maker"
+  :commit "72ce76bb3059e6c715b2471856851aac21198ac7"
+  :revdesc "72ce76bb3059")
