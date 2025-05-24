6caa429ff5a054073da5d8a4a98cd5c39325764a
Saqib Rokadia <saqib@saqibr.com>
2025-05-23 20:38:00 -0700

    Updating emacs packages.

---
+++ emacs/emacs.d/elpa/code-stats-20201209.2135/code-stats-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "code-stats" "20201209.2135"
+  "Code::Stats plugin."
+  '((emacs   "25")
+    (request "0.3.0"))
+  :url "https://github.com/xuchunyang/code-stats-emacs"
+  :commit "9a467dfd6a3cef849468623e1c085cbf59dac154"
+  :revdesc "9a467dfd6a3c"
+  :authors '(("Xu Chunyang" . "mail@xuchunyang.me"))
+  :maintainers '(("Xu Chunyang" . "mail@xuchunyang.me")))
