c88e4d6ba60c24d78779046b793bf1f8f09b285b
Saqib Rokadia <saqib@saqibr.com>
2025-06-01 16:47:36 -0700

    Adding code stats to yaml as well.

--- emacs/emacs.d/srokadia/yaml.el
+++ emacs/emacs.d/srokadia/yaml.el
@@ -2,3 +2,6 @@
 (setq auto-mode-alist (append '(("\\.jinja$" . yaml-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.yaml.template$" . yaml-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.markdown.template$" . markdown-mode)) auto-mode-alist))
+
+(when (auth-source-search :host "codestats.net")
+  (add-hook 'yaml-mode-hook #'code-stats-mode))
