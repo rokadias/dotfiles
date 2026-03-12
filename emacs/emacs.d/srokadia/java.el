d65a2fd40cbb31262d8cfb5a210ec1567f756d77
Saqib Rokadia <saqib@saqibr.com>
2026-03-11 23:22:20 -0700

    Updating for java.

--- emacs/emacs.d/srokadia/java.el
+++ emacs/emacs.d/srokadia/java.el
@@ -1,5 +1,13 @@
 (defun on-java-load ()
-  (eglot-ensure)
+  ;;(lsp)
   )
 
 (add-hook 'java-mode-hook (lambda () (on-java-load)))
+
+(setq lsp-java-configuration-runtimes '[(:name "JavaSE-21"
+					       :path "/usr/lib/jvm/java-21-openjdk"
+                                               :default t)
+					(:name "JavaSE-17"
+					       :path "/usr/lib/jvm/java-17-openjdk")])
+
+(setq lsp-java-java-path "/usr/lib/jvm/java-21-openjdk/bin/java")
