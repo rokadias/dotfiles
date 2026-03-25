2bbf8686f0dc11331af766c238d430488f69e706
Saqib Rokadia <saqib@saqibr.com>
2026-03-25 14:46:34 -0700

    Adding eglot-java.

---
+++ emacs/emacs.d/elpa/eglot-java-20250527.1232/eglot-java-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "eglot-java" "20250527.1232"
+  "Java extension for the eglot LSP client."
+  '((emacs   "26.1")
+    (eglot   "1.0")
+    (jsonrpc "1.0.0"))
+  :url "https://github.com/yveszoundi/eglot-java"
+  :commit "b42b5190f3f59976d330fcec5fd27fc8e2701336"
+  :revdesc "b42b5190f3f5"
+  :keywords '("convenience" "languages")
+  :authors '(("Yves Zoundi and contributors" . "yves_zoundi@hotmail.com"))
+  :maintainers '(("Yves Zoundi" . "yves_zoundi@hotmail.com")))
